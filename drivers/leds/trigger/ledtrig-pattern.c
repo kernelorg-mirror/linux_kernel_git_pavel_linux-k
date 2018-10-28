1f61539d45f7c312eb315e92b8b8a18a8b51544e
Pavel <pavel@ucw.cz>
2018-10-28 15:48:46 +0100

    leds: pattern: More checks would be nice for pattern. Fix transitions in patterns.

--- drivers/leds/trigger/ledtrig-pattern.c
+++ drivers/leds/trigger/ledtrig-pattern.c
@@ -227,9 +227,16 @@ static int pattern_trig_store_patterns_string(struct pattern_trig_data *data,
 
 	while (offset < count - 1 && data->npatterns < MAX_PATTERNS) {
 		cr = 0;
-		ccount = sscanf(buf + offset, "%d %u %n",
+		ccount = sscanf(buf + offset, "%u %u %n",
 				&data->patterns[data->npatterns].brightness,
 				&data->patterns[data->npatterns].delta_t, &cr);
+
+		if (data->patterns[data->npatterns].brightness > data->led_cdev->max_brightness) {
+			data->npatterns = 0;
+			err = -EINVAL;
+			goto out;
+		}
+			 
 		if (ccount != 2) {
 			data->npatterns = 0;
 			return -EINVAL;
